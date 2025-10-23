4144d058a82a79097f2355d012fd33556a7190d6
Oleksandr Kalita <olexadr.kalita@gmail.com>
2025-10-24 00:31:09 +0200

    Fix dotenv for Railway production

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 const express = require('express');
 const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
